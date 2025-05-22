2041bd64cab164c60a3d1a7d21d8fb22f10e429f
Gustavo Tavares <127619878+Tavar3s@users.noreply.github.com>
2025-05-22 15:14:50 -0300

    Its not working
    And i dont know why

---
+++ utilities_c_cpp/src/testes_headers.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "../headers/tools.h"
+
+int main() {
+    char str[100] = "Hello, World!";
+    menu(str);
+    return 0;
+}
---
+++ utilities_c_cpp/src/tools.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "../headers/tools.h"
+
+void menu(char* str){
+    printf("=====================\n");
+    printf("  %s\n", str);
+    printf("=====================\n");
+}
