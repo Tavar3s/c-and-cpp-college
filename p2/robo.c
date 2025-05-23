81f9427b7a0db25c0da0208de049f94a2fffaffc
Gustavo Tavares <127619878+Tavar3s@users.noreply.github.com>
2025-05-23 10:07:59 -0300

    Update p2

--- p2/robo.c
+++ p2/robo.c
@@ -9,13 +9,6 @@ int** aloca_matriz(int linhas, int colunas) {
     return matriz;
 }
 
-void libera_matriz(int** matriz, int linhas) {
-    for (int i = 0; i < linhas; i++) {
-        free(matriz[i]);
-    }
-    free(matriz);
-}
-
 void gerar_imagem(int** imagem, int linhas, int colunas, int largura_trilho) {
     int centro_x = colunas / 2;
     int centro_y = linhas / 2;
@@ -79,32 +72,85 @@ int detectar_caminhos(int** matriz_binaria, int linhas, int colunas) {
     return caminhos;
 }
 
+int* posicao_inicial_caminho(int** matriz_binaria, int linhas, int colunas, int tamanho_vetor) {
+    if (colunas < 10) {
+        return NULL;
+    }
+
+    int* vetor = malloc(tamanho_vetor * sizeof(int));
+    int temp = 0;
+    int count = 0;
+
+    for (int i = 0; i < linhas; i++) {
+        for (int j = 0; j < colunas; j++) {
+            if (matriz_binaria[i][j] == 1) {
+                temp++;
+            } else {
+                temp = 0;
+            }
+            if (temp == 3) {
+                vetor[count] = j-2;
+                count++;
+                break;
+            }
+        } 
+    }
+    
+    return vetor;
+}
+
 int main(void) {
+    // Definindo variáveis
     int linhas, colunas, largura_trilho, caminhos;
     int** imagem = NULL;
     int** matriz_binaria = NULL;
+    void* posicao = NULL;
 
+    // Lendo os parâmetros de entrada
     scanf("%d %d %d", &linhas, &colunas, &largura_trilho);
     if (linhas <= 0 || colunas <= 0 || largura_trilho <= 0) {
         fprintf(stderr, "Erro: Dimensões inválidas.\n");
         return 1;
     }
 
+    // Alocando memória para as matrizes
     matriz_binaria = aloca_matriz(linhas, colunas);
     imagem = aloca_matriz(linhas, colunas);
+
+    // Gerando a imagem e a matriz binária
     gerar_imagem(imagem, linhas, colunas, largura_trilho);
     gerar_matriz_binaria(matriz_binaria, linhas, colunas, imagem);
 
+    // Exibindo a imagem
     for (int i = 0; i < linhas; i++) {
         for (int j = 0; j < colunas; j++) {
             printf("%3d ", imagem[i][j]);
         } printf("\n");
     }
 
+    // Exibindo a imagem com símbolos ASCII
     converter_para_ascii(imagem, linhas, colunas);
 
+    // Detectando caminhos possíveis
     caminhos = detectar_caminhos(matriz_binaria, linhas, colunas);
     printf("Caminhos = %d\n", caminhos);
+
+    // Mostrando as posições iniciais dos caminhos
+    posicao = posicao_inicial_caminho(matriz_binaria, linhas, colunas, caminhos);
+
+    for (int i = 0; i < caminhos; i++){
+        printf("%d ", ((int*)posicao)[i]);
+    } printf("\n");
+
+    // Liberando memória alocada
+    for (int i = 0; i < linhas; i++) {
+        free(imagem[i]);
+        free(matriz_binaria[i]);
+    }
+    free(imagem);
+    free(matriz_binaria);
+    free(posicao);
+
     
     return 0;
 }
