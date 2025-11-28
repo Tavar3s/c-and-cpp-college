8b8bcc9f3018367273d01408fb0ef134d73a04f9
Gustavo Tavares <127619878+Tavar3s@users.noreply.github.com>
2025-11-27 21:18:04 -0300

    Update matriz_esparsa.cpp

--- cpp-classes/aula6/matriz_esparsa.cpp
+++ cpp-classes/aula6/matriz_esparsa.cpp
@@ -5,8 +5,16 @@ class MatrizEsparsa {
         int linhas;
         int colunas;
         float** matriz;
+        int capacidade = 2;
+        int contador = 0;
+        int* linha_valores = new int[capacidade];
+        int* coluna_valores = new int[capacidade];
+        float* valores = new float[capacidade];
 
         MatrizEsparsa(int lin, int col) {
+            if (lin <= 0 || col <= 0) {
+                throw std::invalid_argument("Dimensoes invalidas");
+            }
             linhas = lin;
             colunas = col;
             matriz = new int*[linhas];
@@ -22,7 +30,72 @@ class MatrizEsparsa {
         }
 
         ~MatrizEsparsa() {
+            delete[] linha_valores;
+            delete[] coluna_valores;
+            delete[] valores;
+            for (int i = 0; i < linhas; i++) {
+                delete[] matriz[i];
+            }
             delete[] matriz;
+            std::cout << "Matriz liberada da memoria." << std::endl;
+        }
+
+        void imprimirMatriz() {
+            for (int i = 0; i < linhas; i++) {
+                for (int j = 0; j < colunas; j++) {
+                    std::cout << matriz[i][j] << " ";
+                }
+                std::cout << std::endl;
+            }
+        }
+
+        void imprimirCompressao() {
+            std::cout << "\nTotal de elementos nao-zero: " << contador << std::endl;
+            for (int i = 0; i < contador; i++) {
+                std::cout << "Linha: " << linha_valores[i]
+                          << " Coluna: " << coluna_valores[i]
+                          << " Valor: " << valores[i] << std::endl;
+            }
+        }
+
+        void comprimirMatriz() {
+            int capacidade = 2;
+            contador = 0;
+            linha_valores = new int[capacidade];
+            coluna_valores = new int[capacidade];
+            valores = new float[capacidade];
+
+            for (int i = 0; i < linhas; i++) {
+                for (int j = 0; j < colunas; j++) {
+                    if (matriz[i][j] != 0) {
+                        if (contador >= capacidade) {
+                            int nova_cap = capacidade * 2;
+                            int* linha_temp = new int[nova_cap];
+                            int* coluna_temp = new int[nova_cap];
+                            float* valores_temp = new float[nova_cap];
+
+                            for (int k = 0; k < contador; k++) {
+                                linha_temp[k] = linha_valores[k];
+                                coluna_temp[k] = coluna_valores[k];
+                                valores_temp[k] = valores[k];
+                            }
+
+                            delete[] linha_valores;
+                            delete[] coluna_valores;
+                            delete[] valores;
+                            linha_valores = linha_temp;
+                            coluna_valores = coluna_temp;
+                            valores = valores_temp;
+                            capacidade = nova_cap;
+                        }
+
+                        linha_valores[contador] = i;
+                        coluna_valores[contador] = j;
+                        valores[contador] = static_cast<float>(matriz[i][j]);
+                        contador++;
+                    }
+                }
+            }
         }
 };
 
